729582195323d9035ee9699845f04a754d0ca883
Feng Liu <eric_liufeng@163.com>
2020-03-04 17:33:05 +0800

    回文对 -- 优化算法时间和算法内存，减少复制的次数

--- LeetCode/C/palindromePairs/palindromePairs.c
+++ LeetCode/C/palindromePairs/palindromePairs.c
@@ -36,6 +36,118 @@
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 #define MIN(a, b) ((a) > (b) ? (b) : (a))
 
+#define SIZE 5000
+
+/*
+ * 注意：调用者需要确认拼接的串顺序为，st1 + str2, str2在后面 
+ */
+bool isPalindromeConbineStr(char * str1, char * str2){
+	bool ret = true;
+	int len1 = strlen(str1);
+	int len2 = strlen(str2);
+	char * p1 = NULL, *t1 = NULL;	
+	char * p2 = NULL, *t2 = NULL;
+
+	p1 = str1;
+	if(0 != len1){
+		t1 = p1 + len1 - 1;
+	} else {
+		t1 = p1;
+	}
+
+	p2 = str2;
+	if(0 != len2){
+		t2 = p2 + len2 - 1;
+	} else {
+		t2 = p2;
+	}
+
+	/*比较前缀和后缀部分*/
+	while(p1 != t1 + 1 && p2 - 1 != t2 && '\0' != *p1 && '\0' != *t2) {
+		if(*p1 != *t2){
+			ret = false;
+			goto out;
+		}
+
+		p1++;
+		t2--;
+	} 
+
+	/*比较str1长出的部分*/
+	while(p1 <= t1 && '\0' != *p1) {
+		if(*p1 != *t1){
+			ret = false;
+			goto out;
+		}
+		p1++;
+		t1--;
+	}
+
+	/*比较str2长出的部分*/
+	while(p2 <= t2  && '\0' != *p2) {
+		if(*p2 != *t2){
+			ret = false;
+			goto out;
+		}
+		p2++;
+		t2--;
+	}
+
+out:
+	return ret;
+}
+
+
+
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+int** palindromePairs(char ** words, int wordsSize, int* returnSize, int** returnColumnSizes){
+
+	if(NULL == words || 0 >= wordsSize){
+		*returnColumnSizes = NULL;
+		return NULL;
+	}
+
+	int ** retp = NULL;
+	int i = 0, j = 0, k = 0;
+	int size = 2 * wordsSize;
+
+	retp = (int** )malloc(size * (sizeof(int *)));
+	for(i = 0; i < size; i++){
+		retp[i] = (int *)malloc(10 * sizeof(int)); /*因为每行2个数字进行拼接*/
+	}
+
+	*returnColumnSizes = (int *)malloc(size * sizeof(int));	
+	for(i = 0; i < size; i++){
+		*(*returnColumnSizes + i) = 2; /*因为是两两组合，所以列宽度都是2*/
+	}
+
+	for(i = 0; i < wordsSize; i++){
+		for(j = 0; j < wordsSize; j++){
+			if(i == j){
+				continue;
+			}
+
+			if(isPalindromeConbineStr(words[i], words[j])){
+				retp[k][0] = i;
+				retp[k][1] = j;
+				k++;
+			}			
+		}
+	}
+
+	*returnSize = k;
+
+	return retp;
+
+}
+
+
+#if 0
+
 bool isPalindromeStr(char * head, char * tail){
 	bool ret = true;
 
@@ -53,14 +165,12 @@ bool isPalindromeStr(char * head, char * tail){
 }
 
 
-#define SIZE 100
-
 /**
  * Return an array of arrays of size *returnSize.
  * The sizes of the arrays are returned as *returnColumnSizes array.
  * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
  */
-int** palindromePairs(char ** words, int wordsSize, int* returnSize, int** returnColumnSizes){
+int** palindromePairs2(char ** words, int wordsSize, int* returnSize, int** returnColumnSizes){
 
 	if(NULL == words || 0 >= wordsSize){
 		*returnColumnSizes = NULL;
@@ -117,7 +227,7 @@ int** palindromePairs(char ** words, int wordsSize, int* returnSize, int** retur
 	return retp;
 
 }
-
+#endif
 
 void testpalindromePairs(void){
 	
@@ -129,6 +239,62 @@ void testpalindromePairs(void){
 	int i = 0;
 	int ** ret = NULL;
 	
+#if 1
+	bool bret = false;
+	char * str1 = "lls";
+	char * str2 = "sssll";
+	bret = isPalindromeConbineStr(str1, str2);
+	if(bret){
+		printf("%s + %s is a Palindrome\n", str1, str2);
+	} else {
+		printf("%s + %s not a Palindrome\n", str1, str2);
+	}
+
+	char * str3 = "abcd";
+	char * str4 = "dcba";
+	bret = isPalindromeConbineStr(str1, str2);
+	if(bret){
+		printf("%s + %s is a Palindrome\n", str3, str4);
+	} else {
+		printf("%s + %s not a Palindrome\n", str3, str4);
+	}
+
+	char * str5 = "a";
+	char * str6 = "dcba";
+	bret = isPalindromeConbineStr(str5, str6);
+	if(bret){
+		printf("%s + %s is a Palindrome\n", str5, str6);
+	} else {
+		printf("%s + %s not a Palindrome\n", str5, str6);
+	}
+
+	char * str7 = "";
+	char * str8 = "llsll";
+	bret = isPalindromeConbineStr(str7, str8);
+	if(bret){
+		printf("%s + %s is a Palindrome\n", str7, str8);
+	} else {
+		printf("%s + %s not a Palindrome\n", str7, str8);
+	}
+
+	bret = isPalindromeConbineStr(str8, str7);
+	if(bret){
+		printf("%s + %s is a Palindrome\n", str8, str7);
+	} else {
+		printf("%s + %s not a Palindrome\n", str8, str7);
+	}
+
+
+	char * str9 = "";
+	char * str10 = "";
+	bret = isPalindromeConbineStr(str9, str10);
+	if(bret){
+		printf("%s + %s is a Palindrome\n", str9, str10);
+	} else {
+		printf("%s + %s not a Palindrome\n", str9, str10);
+	}
+#endif
+
 #if 1
 	/*testcase 1*/
 	words = (char** )malloc(SIZE * (sizeof(char *)));
@@ -153,6 +319,116 @@ void testpalindromePairs(void){
 		printf("%d ], ", ret[i][1]);
 	}
 	printf(" ] \n");
+	free(ret);
+	ret = NULL;
+	free(returnColumnSizes);
+	returnColumnSizes = NULL;
+	for(i = 0; i < SIZE; i++){
+		free(words[i]);
+		words[i] = NULL;
+	}	
+	free(words);
+	words = NULL;
+
+	/*testcase 2*/
+	words = (char** )malloc(SIZE * (sizeof(char *)));
+	for(i = 0; i < SIZE; i++){
+		words[i] = (char *)malloc(SIZE * sizeof(char));
+		memset(words[i],'\0', SIZE);
+	}
+	
+	wordsSize = 2;
+	memcpy(words[0], "", 0);
+	memcpy(words[1], "a", 1);
+
+	ret = palindromePairs(words, wordsSize, &returnSize, &returnColumnSizes);
+	printf("-------------  returnSize = %d -------------\n", returnSize);
+	printf("\t[ ");
+	for(i = 0; i < returnSize; i++){
+		printf(" [ ");
+		printf("%d,", ret[i][0]);
+		printf("%d ], ", ret[i][1]);
+	}
+	printf(" ] \n");
+	free(ret);
+	ret = NULL;
+	free(returnColumnSizes);
+	returnColumnSizes = NULL;
+	for(i = 0; i < SIZE; i++){
+		free(words[i]);
+		words[i] = NULL;
+	}	
+	free(words);
+	words = NULL;
+
+
+	/*testcase 3*/
+	words = (char** )malloc(SIZE * (sizeof(char *)));
+	for(i = 0; i < SIZE; i++){
+		words[i] = (char *)malloc(SIZE * sizeof(char));
+		memset(words[i],'\0', SIZE);
+	}
+	
+	wordsSize = 6;
+	memcpy(words[0], "a", 1);
+	memcpy(words[1], "b", 1);
+	memcpy(words[2], "c", 1);
+	memcpy(words[3], "ab", 2);
+	memcpy(words[4], "ac", 2);
+	memcpy(words[5], "aa", 2);
+	
+	ret = palindromePairs(words, wordsSize, &returnSize, &returnColumnSizes);
+	printf("-------------  returnSize = %d -------------\n", returnSize);
+	printf("\t[ ");
+	for(i = 0; i < returnSize; i++){
+		printf(" [ ");
+		printf("%d,", ret[i][0]);
+		printf("%d ], ", ret[i][1]);
+	}
+	printf(" ] \n");
+	free(ret);
+	ret = NULL;
+	free(returnColumnSizes);
+	returnColumnSizes = NULL;
+	for(i = 0; i < SIZE; i++){
+		free(words[i]);
+		words[i] = NULL;
+	}	
+	free(words);
+	words = NULL;
+
+
+	/*testcase 4*/
+	words = (char** )malloc(SIZE * (sizeof(char *)));
+	for(i = 0; i < SIZE; i++){
+		words[i] = (char *)malloc(SIZE * sizeof(char));
+		memset(words[i],'\0', SIZE);
+	}
+	
+	wordsSize = 3;
+	memcpy(words[0], "a", 1);
+	memcpy(words[1], "aa", 2);
+	memcpy(words[2], "aaa", 3);
+	
+	ret = palindromePairs(words, wordsSize, &returnSize, &returnColumnSizes);
+	printf("-------------  returnSize = %d -------------\n", returnSize);
+	printf("\t[ ");
+	for(i = 0; i < returnSize; i++){
+		printf(" [ ");
+		printf("%d,", ret[i][0]);
+		printf("%d ], ", ret[i][1]);
+	}
+	printf(" ] \n");
+	free(ret);
+	ret = NULL;
+	free(returnColumnSizes);
+	returnColumnSizes = NULL;
+	for(i = 0; i < SIZE; i++){
+		free(words[i]);
+		words[i] = NULL;
+	}	
+	free(words);
+	words = NULL;
 
 #endif
 
