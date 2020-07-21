60633a61c2b2f98fb8920071c3278351f7937718
Feng Liu <eric_liufeng@163.com>
2020-07-21 14:51:18 +0800

    增强希尔排序注释，说明分组和有序区，无序区的位置

--- CCPPBible/src/sort/ShellSort.c
+++ CCPPBible/src/sort/ShellSort.c
@@ -19,17 +19,25 @@ int ShellSort(int * array, int size){
 	int k = 0; /*debug 输出*/
 #endif
 
+	/*
+	 * 注意：分组是以D为步长，跳着分组的
+	 * 【0，D-1，2D-1, 3D-1】 为一组
+	 *  不是连续的【0~D】为一组！
+     */
+
 	for(dist = size/2; dist > 0; dist--){ /*步距逐步减小*/		
 #ifdef DEBUG		
 		printf("\n ========  distance = %d =============\n", dist);
 #endif		
 		for(m =0; m < dist; m++) { //当前dist下，分别插入排序 dist -1 组
 
-			/*对每组进行插入排序*/
+			/*对每组进行插入排序
+			 * i 以后是无序区，步长为dist
+		     */
 			for(i = m; i < size ; i = i + dist) {
 				insert = array[i];
 
-				/*有序区, 没有找到合适的位置，向后移动*/
+				/*i前面是有序区, 没有找到合适的位置，向后移动步长dist*/
 				for(j = i - dist; j >= 0; j = j - dist){
 					if(array[j] >= insert){
 						array[j + dist] = array[j];
