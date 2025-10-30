aa4dd262ac5370bc365c3f29bf41669144103e0a
Feng Liu <feliu@nvidia.com>
2025-10-30 14:13:13 -0400

    list
    Issue:
    Signed-off-by: Feng Liu <feliu@nvidia.com>

--- Algorithm/src/struct/list.c
+++ Algorithm/src/struct/list.c
@@ -361,8 +361,8 @@ int merge_sorted_lists(ListNode **head1, ListNode *head2) {
 
     if (*head1 == NULL) {
         *head1 = head2;
-        return SUCCESS;
-    }
+	return SUCCESS;
+}
 
     if (head2 == NULL) {
 	return SUCCESS;
